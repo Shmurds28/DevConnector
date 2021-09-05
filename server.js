6511864a9679be4ad4a52c649bbd54f8c943ba88
Simamkele <simamkelengcasane28@gmail.com>
2021-09-06 00:11:05 +0200

    Update database connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express  = require('express');
 const connectDB = require('./config/db');
+const path = require("path");
 
 const app = express();
 
@@ -8,6 +9,7 @@ connectDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get('/', (req, res) => {
     res.send('API RUNNING');
@@ -18,6 +20,10 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
